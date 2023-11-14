d4a53f8193fd6ad44f0ca75d19c357582c6a0079
Letícia <leti.morais48@gmail.com>
2023-11-14 15:12:00 -0300

    doc: added docstrings

--- logins.py
+++ logins.py
@@ -1,8 +1,24 @@
-def loginGerente():
-    pass
-
-def loginFuncionario():
-    pass
+from formatacao import *
 
 def cadastroFuncionario():
-    pass
+    """Cumpre o requisito [RF002] Cadastro Funcionário(a):\n
+    O sistema deve possibilitar que o funcionário(a) realize o cadastro a partir de suas credenciais a seguir: nome completo, e-mail, telefone, cidade/estado, CPF e data de nascimento e uma senha.\n
+    PRIORIDADE: ( ) Essencial; (X ) Importante; ( ) Desejável.
+    """
+    
+    nomeMenu("Cadastro de Funcionário")
+
+def loginGerente():
+    """Cumpre o requisito [RF003] Login gerente:\n
+    O sistema deve possibilitar que o gerente realize cadastro no sistema com seu CPF e sua senha.\n
+    PRIORIDADE: ( ) Essencial; ( ) Importante; (X) Desejável.
+    """
+
+    nomeMenu("Login do Gerente")
+    
+def loginFuncionario():
+    """Cumpre o requisito [RF004] Login Funcionário(a):\n
+    O sistema deve possibilitar que o funcionário(a) realize login no sistema com suas credenciais de CPF e senha.\n
+    PRIORIDADE: ( ) Essencial; (X) Importante; ( ) Desejável.
+    """
+    nomeMenu("Login do Funcionário")
