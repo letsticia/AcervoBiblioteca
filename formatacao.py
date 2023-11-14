59666ac679a21ca92ef84a5836cf10a4e3506240
Letícia <leti.morais48@gmail.com>
2023-11-14 15:34:48 -0300

    feat: added centralized input

--- formatacao.py
+++ formatacao.py
@@ -15,3 +15,16 @@ def centralizado(string):
         string (str): string a ser centralizada
     """
     print(f"{' ':>15}{string}")
+
+def inputCentralizado(string):
+    """Formata o input
+
+    Args:
+        string (str): string que ficará dentro do input
+
+    Returns:
+        str : resultado do input
+    """
+    dado = input(f"{' ':>10}{string}")
+    
+    return dado
