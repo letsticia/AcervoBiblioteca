a6503db5dc36b2cd0aa6cf7ae1556fd35dc53e47
Letícia <leti.morais48@gmail.com>
2023-11-15 12:36:36 -0300

    feat: added cities and states

--- funcionario.py
+++ funcionario.py
@@ -1,9 +1,10 @@
 class Funcionario:
-    def __init__(self, nome, email, telefone, cep, cpf, nascimento, senha):
+    def __init__(self, nome, email, telefone, cidade, estado, cpf, nascimento, senha):
         self.nome = nome.title()
         self.email = email
-        self.telefone = telefone
-        self.cep = cep
+        self.telefone = int(telefone)
+        self.cidade = cidade
+        self.estado = estado
         self.cpf = cpf
         self.nascimento = nascimento
         self.senha = senha
