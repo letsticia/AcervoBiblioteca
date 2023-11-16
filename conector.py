27a8901e0bac1cde8f02501ea415a9a6b3637ec1
Letícia <leti.morais48@gmail.com>
2023-11-16 17:49:20 -0300

    feat: added manager db conector

--- conector.py
+++ conector.py
@@ -1,5 +1,19 @@
 from tinydb import TinyDB
 
+# conectando o tinyDB ao banco de dados geral
 db = TinyDB('db.json')
+
+# definindo a tabela de solicitação de funcionários
 tabelaSolicitacaoFuncionarios = db.table("SolicitacaoFuncionarios")
+
+# definindo a tabela de Funcionários
 tabelaFuncionarios = db.table("Funcionários")
+
+# conectando o tinyDB ao banco de dados do gerente
+dbGerente = TinyDB('gerente.json')
+
+# definindo a tabela Gerente
+tabelaGerente = dbGerente.table("gerente")
+
+# dicionário com o email e a senha do gerente
+infoGerente = tabelaGerente.all()[0]
