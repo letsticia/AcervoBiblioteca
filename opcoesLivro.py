22520243a61e6ebc0393a4d4fb63e4786632cba7
Letícia <leti.morais48@gmail.com>
2023-11-20 19:51:36 -0300

    feat: added book search options

--- opcoesLivro.py
+++ opcoesLivro.py
@@ -41,7 +41,21 @@ def cadastraLivro():
     cadastro = infoFuncionarioOnline['nome']
     
     # criando o objeto livro
-    novoLivro = Livro(nome, autor, editora, ano, cadastro)
+    novoLivro = Livro(nome, autor, editora, ano, cadastro, "Disponivel")
     
     # colocando o objeto no banco de dados
     tabelaLivros.insert(novoLivro.__dict__)
+    
+def menuBuscaLivro():
+    """Cumpre o requisito [RF011] Buscar livro \n
+    O sistema deve possibilitar que o funcionário(a) busque o livro ou no caso filtre a busca do
+    livro a partir de qualquer uma de suas credenciais.\n
+    PRIORIDADE: (X) Essencial; ( ) Importante; ( ) Desejável.
+    """
+    nomeMenu("Buscar um livro")
+    
+    centralizado("(1)   Procurar por nome")
+    centralizado("(2)   Procurar por autor")
+    centralizado("(3)   Procurar por data de lançamento")
+    centralizado("(4)   Procurar por editora")
+    centralizado("(5)   Voltar ao menu de Funcionários")
