c4ebfc0241a05f2568f2adcbf1c41b167ed89585
Letícia <leti.morais48@gmail.com>
2023-11-21 17:34:03 -0300

    feat: added book's ID

--- livro.py
+++ livro.py
@@ -3,11 +3,11 @@
 from formatacao import substituiAcento
 
 class Livro:
-    def __init__(self, nome, autor, editora, ano, cadastro, status):
+    def __init__(self, nome, autor, editora, ano, cadastro, status, numID):
         self.nome = substituiAcento(nome.title())
         self.autor = substituiAcento(autor.title())
         self.editora = substituiAcento(editora.title())
         self.ano = ano
         self.cadastro = cadastro
         self.status = status
-        
+        self.numID = numID
