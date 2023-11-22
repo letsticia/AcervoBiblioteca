9329a93137260f163ff97895d4b85b3f95970cc2
Letícia <leti.morais48@gmail.com>
2023-11-22 14:34:58 -0300

    style: blank spaces

--- opcoesLivro.py
+++ opcoesLivro.py
@@ -12,25 +12,25 @@ def cadastraLivro():
     
     nomeMenu("Cadastro de Livro")
     
-    nome = inputCentralizado(f"[{'Nome':<29}]")
+    nome = inputCentralizado(f"[{'Nome':<29}]: ")
     
     # validando o nome
     if not validaObrigatoriedade(nome):
         return cadastraLivro()
     
-    autor = inputCentralizado(f"[{'Autor':<29}]")
+    autor = inputCentralizado(f"[{'Autor':<29}]: ")
     
     # validando autor
     if not validaObrigatoriedade(autor):
         return cadastraLivro()
     
-    editora = inputCentralizado(f"[{'Editora':<29}]")
+    editora = inputCentralizado(f"[{'Editora':<29}]: ")
     
     # validando editora
     if not validaObrigatoriedade(editora):
         return cadastraLivro()
     
-    ano = inputCentralizado(f"[{'Ano de Publicacao (4 dígitos)':<29}]")
+    ano = inputCentralizado(f"[{'Ano de Publicacao (4 dígitos)':<29}]: ")
     
     # validando o ano de publicacao
     if not validaAno(ano):
